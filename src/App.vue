b0abf0369675047940885a4af722bd5c094856bc
WanxinXie <wanxinxie719@g.ucla.edu>
2020-03-27 23:48:37 -0700

    intro page first version

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <IntroPage></IntroPage>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IntroPage from './components/IntroPage.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+ IntroPage
   }
+
 }
 </script>
 
